a27baae4696f8fd4f8ca1c96c19545bbacd915d9
Lukas Scholz <40420097+therealgherkhin@users.noreply.github.com>
2020-04-14 13:31:30 +0200

    Added Diverses1.py

--- Diverses1.py
+++ Diverses1.py
@@ -2,9 +2,30 @@
 
 #Funktionsparameter benennen 
 
+"""
 def multi_print(number = 3, word = "Hallo"):
     for i in range(0, number):
         print(word)
 
 multi_print()
 multi_print(5, "Welt")
+"""
+
+#Wie werden Funktionsparameter übergeben
+
+a = 5
+l = ["Hallo", "Welt"]
+
+def f(x):
+    #x.append("111")     #Kopie von der Liste l wird angelegt und darin wird der Inhalt verändert
+    print(x)
+
+f(l)
+print(l)    #da in der Funktion die Kopie verändert worden ist wird hier auch die veränderte Kopie ausgegeben
+
+def j(p):
+    p = ["Hallo", "Welt", "!!!"]    #Es wird eine neue Kopie erstellt und die andere wird ebenfalls erhalten
+    print(p)
+
+j(l)        #Die neue Kopie wird ausgegeben
+print(l)    #Die alter Version wird ausgegeben
